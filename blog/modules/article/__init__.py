185e93d47584b13e4b26cbca6668a8a8ea6c512a
胡亮 <silencehuliang@163.com>
2021-04-21 15:27:56 +0800

    fix bug

--- blog/modules/article/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-article_bp = Blueprint('article', __name__, url_prefix='/article')
-
-from . import views
--- blog/modules/index/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-index_bp = Blueprint('index', __name__, )
-
-from . import views
